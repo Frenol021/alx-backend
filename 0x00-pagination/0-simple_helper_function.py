7dad89109beee07b8dd6c12118d2df7a78172c90
frenol021 <frenol021@gmail.com>
2023-07-23 10:21:56 +0000

    tuple

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,4 +4,6 @@ Simple helper function
 """
 
 def index_range(page, page_size):
-    return(page, page_size)
+    start_index = (page -1) * page_size
+    end_index = start_index + page_size
+    return(start_index, end_index)
